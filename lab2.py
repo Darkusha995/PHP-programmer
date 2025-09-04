66eb5de32f7b0d1f3b10aa40ae2a97fb0b0bccc3
Darkusha995 <ajdarskr@gmail.com>
2025-09-04 09:05:36 +0500

    Лаб работа №1

---
+++ lab2.py
@@ -0,0 +1,33 @@
+#лабораторная работа №2
+# вычитание 
+apples = 10
+eaten = 3
+print("Осталось яблок:", apples - eaten)   # 7
+
+# сложение 
+morning_steps = 1234
+evening_steps = 4567
+print("Суммарные шаги за день:", morning_steps + evening_steps)
+
+# умножение 
+price = 499
+items = 4
+print("Стоимость покупки:", price * items, "₸")
+
+# возведение в степень 
+side = 5
+print("Площадь квадрата:", side ** 2)
+
+# деление 
+distance = 120
+time = 2
+print("Скорость:", distance / time, "км/ч")
+
+# целочисленное деление 
+minutes = 135
+print("Целых часов прошло:", minutes // 60)
+
+# остаток от деления 
+candies = 23
+friends = 5
+print("Остаток конфет после дележа:", candies % friends)
